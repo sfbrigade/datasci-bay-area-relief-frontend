d15c97aab28259fcf89fc6d3463f30bc603f1b99
Joey Leung <joeyleung8@gmail.com>
2020-06-27 11:32:15 -0700

    Split Home view into flex container and added first SVG

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,30 @@
-import {Link} from "react-router-dom";
 import React from "react";
+import {Link} from "react-router-dom";
+import styled from 'styled-components';
+import { ReactComponent as CoolIllustration } from "./assets/CoolIllustration.svg"
 
-const Home = () => <div>
-  <h1>Find Loans & Grants</h1>
-  <Link to="/results">Go</Link>
-</div>;
+const Container = styled.div`
+  display: flex;
+`;
+
+const Section = styled.div`
+  flex: 1 1 auto;
+`;
+
+const PlaceholderIllustration = styled(CoolIllustration)`
+  position: absolute;
+  width: 823px;
+  height: 823px;
+  left: -201px;
+  top: 128px;
+`;
+
+const Home: React.FC = () => <Container>
+  <Section><PlaceholderIllustration/></Section>
+  <Section>
+    <h1>Find Loans & Grants</h1>
+    <Link to="/results">Go</Link>
+  </Section>
+</Container>;
 
 export default Home;
