3abebe73c57d98b713be121f822b256bb113cef0
Enrique <enriquecardenas96@gmail.com>
2020-06-29 18:01:59 -0700

    Add first dropdown for selecting business type

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import {Link} from "react-router-dom";
 import styled from 'styled-components';
 import { ReactComponent as CoolIllustration } from "./assets/CoolIllustration.svg"
 import Button from '@material-ui/core/Button';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 const Container = styled.div`
   display: flex;
+  height: 100vh;
 `;
 
 const Section = styled.div`
   flex: 1 1 auto;
 `;
 
+const StyledFormControl = styled(FormControl)`
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+`;
 const PlaceholderIllustration = styled(CoolIllustration)`
   position: absolute;
   width: 823px;
@@ -20,12 +29,44 @@ const PlaceholderIllustration = styled(CoolIllustration)`
   top: 128px;
 `;
 
-const Home: React.FC = () => <Container>
-  <Section><PlaceholderIllustration/></Section>
-  <Section>
-    <h1>Find Loans & Grants</h1>
-    <Button variant="contained" component={Link} to='/results'>Go</Button>
-  </Section>
-</Container>;
+const Home: React.FC = () => {
+  const [ businessType, setBusinessType ] = useState('')
+
+  const handleBusinessTypeChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setBusinessType(event.target.value as string);
+  }
+
+  return (
+    <Container>
+      <Section><PlaceholderIllustration/></Section>
+      <Section>
+        <h1>Find Loans & Grants</h1>
+        <p>Search our database for Bay Area loans</p>
+        
+        <StyledFormControl variant="outlined">
+          <InputLabel htmlFor='business-type-select'>I am a...</InputLabel>
+          <Select
+            native
+            value={businessType}
+            onChange={handleBusinessTypeChange}
+            name='business-type'
+            inputProps={{
+              name: 'business-type',
+              id: 'business-type-select', 
+              'data-testid': 'business-type-select'
+            }}
+          >
+            <option aria-label="None" value='' />
+            <option value='small-business'>Small business</option>
+            <option value='non-profit'>Non-profit</option>
+          </Select>
+
+          <Button variant="contained" component={Link} to='/results'>Go</Button>
+        </StyledFormControl>
+
+      </Section>
+    </Container>
+  );
+}
 
 export default Home;
