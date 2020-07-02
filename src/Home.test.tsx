2b22cf31e1e32a4e11f5bc146f7578cb38493f46
Enrique Cardenas <enriquecardenas96@gmail.com>
2020-07-01 19:24:05 -0700

    Dropdown form (#3)
    * Add first dropdown for selecting business type
    * Add test selecting business type
    * Update business type dropdown test name

--- src/Home.test.tsx
+++ src/Home.test.tsx
@@ -16,8 +16,18 @@ describe('Home', () => {
     )
   });
 
-  it('renders a homepage title', () => {
+  it('renders a homepage title with description', () => {
     expect(screen.getByRole('heading')).toHaveTextContent('Find Loans & Grants');
+    expect(screen.getByText('Search our database for Bay Area loans')).toBeInTheDocument();
+  });
+
+  it('displays a business type dropdown that changes values on different options',  () => {  
+    const businessTypeSelect = screen.getByTestId('business-type-select')
+    expect(businessTypeSelect).toHaveValue('')
+    
+    fireEvent.change(businessTypeSelect, { target: { value: 'small-business' }})
+    expect(businessTypeSelect).toHaveValue('small-business')
+
   });
 
   it('should have a Go button that takes the user to the results page', () => {
