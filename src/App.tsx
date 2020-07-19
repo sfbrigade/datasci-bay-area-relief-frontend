478425ef34c96c4d85a2c7b9272c8bedef483828
Berezka-creator <61727413+Berezka-creator@users.noreply.github.com>
2020-07-19 00:21:04 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ const App = () => {
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/Aboutus.tsx" component={Aboutus}/>
-          <Route exact path="/HowItWorks.tsx" component={HowItWorks}/>
+          <Route exact path="/Aboutus" component={Aboutus}/>
+          <Route exact path="/HowItWorks" component={HowItWorks}/>
 
           <Route exact path="/results" component={Results} />
         </Switch>
