5cc2dbf37089391df41abc1697242f67072de283
Mejin Leechor <amleechor@gmail.com>
2020-09-16 21:52:20 -0700

    Self-host fonts

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import "./assets/fonts/bree-serif.css";
+import "./assets/fonts/source-sans-pro.css";
+import {createMuiTheme} from "@material-ui/core/styles";
+
+export const theme = createMuiTheme({
+  typography: {
+    fontFamily: "Source Sans Pro",
+  },
+});
