78598db0f9fabcf4ff2af1c53bfe1a75286f53f0
Keegan Holley <keegan2149@gmail.com>
2021-03-14 21:47:10 -0400

    Initial Form design and styling

--- src/components/donate/Donate.tsx
+++ src/components/donate/Donate.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-
 import Typography from "@material-ui/core/Typography";
-import Link from '@material-ui/core/Link';
+import DonateForm from "./DonateForm";
 
 
 const Container = styled.div`
@@ -61,95 +60,8 @@ const Donate: React.FC = () => {
       <TopSection>
         <Typography variant="h3">Donate</Typography>
         <br />
-        <TopSectionBody> 
-          Not a small business, but want to help? We’ve collected a list of
-          funds you can donate to.
-        </TopSectionBody>
       </TopSection>
-
-      <Section>
-        <SectionHeader>General</SectionHeader>
-        <DonationList>
-          <ListItem>
-            <Link variant="h6" href="https://www.gofundme.com/">
-              GoFundMe&apos;s in the Bay area
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link variant="h6" href="https://www.redwoodcity.org/home">
-              Redwood City Small Business Relief Fund{" "}
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link variant="h6" href="#">UC Berkeley Law Pro Bono</Link>
-          </ListItem>
-          <ListItem>
-            <Link variant="h6" href="#">COVID-19 Regional Response Fund</Link>
-          </ListItem>
-          <ListItem>
-            <Link variant="h6" href="#">Norcal Small Business Relief Fund</Link>
-          </ListItem>
-          <ListItem>
-            <Link variant="h6" href="#">One Fair Wage </Link>
-          </ListItem>
-          <ListItem>
-            <Link variant="h6" href="#">Berkeley Relief Fund</Link>
-          </ListItem>
-          <ListItem>
-            <Link variant="h6" href="#">Berkeley Mutual Aid</Link>
-          </ListItem>
-          <ListItem>
-            <Link variant="h6" href="#">Save Our China Towns </Link>
-          </ListItem>
-          <ListItem>
-            <Link variant="h6" href="#">MEDA (Mission Economic Development Agency)</Link>
-          </ListItem>
-        </DonationList>
-      </Section>
-
-      <Section>
-        <SectionHeader>
-          Black Lives
-          <br /> Matter
-        </SectionHeader>
-        <DonationList>
-          <ListItem>
-            <Link variant="h6" href="#">YouTube Ad Proceeds to #BLM</Link>
-          </ListItem>
-          <ListItem>
-            <Link variant="h6" href="#">
-              Bay Area Organization of Black Owned Businesses (BAOBOB)
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link variant="h6" href="#">Black Owned Restaurants</Link>
-          </ListItem>
-          <ListItem>
-            <Link variant="h6" href="#">Black Owned Business ReliefFund</Link>
-          </ListItem>
-        </DonationList>
-      </Section>
-
-      <Section>
-        <SectionHeader>LGBT</SectionHeader>
-        <DonationList>
-          <ListItem>
-            <Link variant="h6" href="#">Silicon Valley Central Chamber of Commerce</Link>
-          </ListItem>
-          <ListItem>
-            <Link variant="h6" href="#">SF Gov</Link>
-          </ListItem>
-          <ListItem>
-            <Link variant="h6" href="#">Queer: Way Out</Link>
-          </ListItem>
-          <ListItem>
-            <Link variant="h6" href="#">James Beard Foundation</Link>
-          </ListItem>
-          <ListItem>
-            <Link variant="h6" href="#">James Beard Webinars</Link>
-          </ListItem>
-        </DonationList>
-      </Section>
+      <DonateForm />
     </Container>
   );
 };
