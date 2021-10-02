fe6ce241602ae90e4d7bca95fc7b2efcd37dfab3
adamos486 <adamos486@gmail.com>
2021-10-02 10:30:39 -0700

    Refactoring Styled Components from ResultCard into ResultCard.styles.tsx

---
+++ src/components/results/ResultCard.styles.tsx
@@ -0,0 +1,83 @@
+import styled from "styled-components";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+
+export const StyledCard = styled(Card)`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  flex-basis: auto;
+  background: #ffffff;
+  border-radius: 4px;
+  width: 300px;
+  height: 172px;
+  margin: 1.5em;
+
+  @media (min-width: 752px) {
+    width: 500px;
+  }
+`;
+
+export const SupportTypeContainer = styled.div`
+  display: flex;
+  position: absolute;
+  width: 80%;
+  left: 16px;
+  top: 15px;
+`;
+
+export const SupportTypeItem = styled(Typography).attrs({variant: 'overline' })`
+  position: static;
+`;
+
+export const StyledAwardAmount = styled(Typography).attrs({variant: 'h5' })`
+  margin: 0;
+  padding: 0;
+  text-align: left;
+  position: absolute;
+  width: 90%;
+  left: 16px;
+  top: 38px;
+`;
+
+export const StyledName = styled(Typography).attrs({variant: 'body2' })`
+  text-align: left;
+  position: absolute;
+  width: 80%;
+  left: 16px;
+  top: 68px;
+  color: rgba(0, 0, 0, 0.6);
+`;
+
+export const StyledReliefType = styled(Typography).attrs({variant: 'body2' })`
+  text-align: left;
+  position: absolute;
+  left: 16px;
+  top: 100px;
+  color: rgba(0, 0, 0, 0.38);
+`;
+
+export const CardBottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: absolute;
+  bottom: 12px;
+  width: 95%;
+  left: 12px;
+`;
+
+export const Tags = styled.div`
+  width: 60%;
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 10px
+`;
+
+export const Tag = styled(Typography).attrs({variant: 'body2' })`
+  background: linear-gradient(0deg, #fece4b, #fece4b);
+  border-radius: 16px;
+  padding-left: 12px;
+  padding-right: 12px;
+  color: rgba(0, 0, 0, 0.87);
+`;
--- src/components/results/ResultCard.tsx
+++ src/components/results/ResultCard.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import styled from "styled-components";
 import {Result} from "../../types";
-import Card from "@material-ui/core/Card";
 import {
   formatAwardAmount,
   formatDate,
@@ -9,87 +7,15 @@ import {
   formatReliefType,
 } from "./formatHelpers";
 import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-
-const StyledCard = styled(Card)`
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  flex-basis: auto;
-  background: #ffffff;
-  border-radius: 4px;
-  width: 300px;
-  height: 172px;
-  margin: 1.5em;
-
-  @media (min-width: 752px) {
-    width: 500px;
-  }
-`;
-
-const SupportTypeContainer = styled.div`
-  display: flex;
-  position: absolute;
-  width: 80%;
-  left: 16px;
-  top: 15px;
-`;
-
-const SupportTypeItem = styled(Typography).attrs({variant: 'overline' })`
-  position: static;
-`;
-
-const StyledAwardAmount = styled(Typography).attrs({variant: 'h5' })`
-  margin: 0;
-  padding: 0;
-  text-align: left;
-  position: absolute;
-  width: 90%;
-  left: 16px;
-  top: 38px;
-`;
-
-const StyledName = styled(Typography).attrs({variant: 'body2' })`
-  text-align: left;
-  position: absolute;
-  width: 80%;
-  left: 16px;
-  top: 68px;
-  color: rgba(0, 0, 0, 0.6);
-`;
-
-const StyledReliefType = styled(Typography).attrs({variant: 'body2' })`
-  text-align: left;
-  position: absolute;
-  left: 16px;
-  top: 100px;
-  color: rgba(0, 0, 0, 0.38);
-`;
-
-const CardBottom = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: absolute;
-  bottom: 12px;
-  width: 95%;
-  left: 12px;
-`;
-
-const Tags = styled.div`
-  width: 60%;
-  display: flex;
-  justify-content: flex-end;
-  padding-right: 10px
-`;
-
-const Tag = styled(Typography).attrs({variant: 'body2' })`
-  background: linear-gradient(0deg, #fece4b, #fece4b);
-  border-radius: 16px;
-  padding-left: 12px;
-  padding-right: 12px;
-  color: rgba(0, 0, 0, 0.87);
-`;
+import {
+  CardBottom,
+  StyledAwardAmount,
+  StyledCard,
+  StyledName,
+  StyledReliefType,
+  SupportTypeContainer,
+  SupportTypeItem, Tag, Tags
+} from "./ResultCard.styles";
 
 const onApply = (url: string) => {
   window.open(url, "_blank");
