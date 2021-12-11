b5d27ccd5c02e42a992e41c244bfc8a6503aa0e8
alex94143 <89117842+alex94143@users.noreply.github.com>
2021-12-10 18:32:39 -0800

    refactor: donate styles into separate file (#42)

---
+++ src/components/donate/Donate.styles.ts
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import Typography from "@material-ui/core/Typography";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #fafafa;
+  min-height: 100vh;
+`;
+
+export const TopSection = styled.div`
+  text-align: center;
+  width: 708px;
+  padding-top: 120px;
+`;
+
+export const TopSectionBody = styled(Typography).attrs({variant: "body1"})`
+  color: rgba(0, 0, 0, 0.8);
+  margin: 0px;
+  padding: 0px;
+`;
+
+export const Section = styled.div`
+  width: 714px;
+  display: flex;
+  text-align: center;
+  margin-top: 44px;
+`;
+
+export const SectionHeader = styled(Typography).attrs({variant: "h4"})`
+  display: flex;
+  flex: 1 1 0;
+  margin: 0px;
+  width: 38%;
+  text-align: left;
+`;
+
+export const DonationList = styled.ul`
+  width: 62%;
+  list-style: none;
+  text-align: left;
+  padding: 0px;
+  margin: 0px;
+`;
+
+export const ListItem = styled.li`
+  padding: 0px;
+  margin: 0px;
+  color: #ef5350;
+`;
--- src/components/donate/Donate.tsx
+++ src/components/donate/Donate.tsx
@@ -1,58 +1,11 @@
 import React from "react";
-import styled from "styled-components";
 
-import Typography from "@material-ui/core/Typography";
 import Link from "@material-ui/core/Link";
 import {Button} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background: #fafafa;
-  min-height: 100vh;
-`;
+import {Container, TopSection, TopSectionBody, Section, SectionHeader, DonationList, ListItem} from './Donate.styles';
 
-const TopSection = styled.div`
-  text-align: center;
-  width: 708px;
-  padding-top: 120px;
-`;
-
-const TopSectionBody = styled(Typography).attrs({variant: "body1"})`
-  color: rgba(0, 0, 0, 0.8);
-  margin: 0px;
-  padding: 0px;
-`;
-
-const Section = styled.div`
-  width: 714px;
-  display: flex;
-  text-align: center;
-  margin-top: 44px;
-`;
-
-const SectionHeader = styled(Typography).attrs({variant: "h4"})`
-  display: flex;
-  flex: 1 1 0;
-  margin: 0px;
-  width: 38%;
-  text-align: left;
-`;
-
-const DonationList = styled.ul`
-  width: 62%;
-  list-style: none;
-  text-align: left;
-  padding: 0px;
-  margin: 0px;
-`;
-
-const ListItem = styled.li`
-  padding: 0px;
-  margin: 0px;
-  color: #ef5350;
-`;
 
 const Donate: React.FC = () => {
   return (
