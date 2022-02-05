e34153dcd5f5c9d3626fd2d33c0efcb9508f89ee
Alex <alex94143@yahoo.com>
2022-02-05 13:39:41 -0800

    fix: remove prettyDom from imports

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {render, screen, prettyDOM} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import Header from "./Header";
 import {Router} from "react-router-dom";
 import {createMemoryHistory} from "history";
