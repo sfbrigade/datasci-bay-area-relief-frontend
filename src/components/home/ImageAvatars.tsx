f176f5e5f663f8907054a731e06688ce49527b56
Adam Cobb <adamos486@gmail.com>
2022-03-16 20:00:22 -0700

    feat: adding alex to the About component

--- src/components/home/ImageAvatars.tsx
+++ src/components/home/ImageAvatars.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Avatar from '@material-ui/core/Avatar';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
-import rocio from '../../assets/avatars/rocio.jpg';
-import emily from '../../assets/avatars/emily.jpg';
+import rocio from "../../assets/avatars/rocio.jpg";
+import emily from "../../assets/avatars/emily.jpg";
+import alex from "../../assets/avatars/alexkerr.jpg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    '& > *': {
+    display: "flex",
+    "& > *": {
       margin: theme.spacing(1),
     },
   },
@@ -27,30 +28,46 @@ export default function ImageAvatars() {
   return (
     <Box component="section" mt={8} mb={8}>
       <Typography variant="h3">Meet the Team</Typography>
-    <div className={classes.root}>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Box display="flex"  
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center">
-        <Avatar alt="Rocio" src={rocio} className={classes.large} />
-        <Typography>Rocio Ng</Typography>
-        <Typography variant="body2">Project Lead</Typography>
-        </Box>
+      <div className={classes.root}>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Avatar alt="Rocio" src={rocio} className={classes.large} />
+              <Typography>Rocio Ng</Typography>
+              <Typography variant="body2">Project Lead</Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={6}>
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Avatar alt="Emily" src={emily} className={classes.large} />
+              <Typography>Emily Kasa</Typography>
+              <Typography variant="body2">Data Jam Lead</Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={6}>
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Avatar alt="Alex" src={alex} className={classes.large}/>
+              <Typography>Alex Kerr</Typography>
+              <Typography variant="body2">Engineer</Typography>
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-        <Box display="flex"  
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center">
-        <Avatar alt="Emily" src={emily} className={classes.large} />
-        <Typography>Emily Kasa</Typography>
-        <Typography  variant="body2">Data Jam Lead</Typography>
-        </Box>
-        </Grid>
-      </Grid>
-    </div>
+      </div>
     </Box>
   );
 }
