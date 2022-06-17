add7eb6c9a840f7b173767a1b56b46235d897c28
hayitsdavid <davehay93@gmail.com>
2022-06-16 18:37:59 -0700

    chore: cleanup unused filter code

--- src/components/results/FilterBar.tsx
+++ src/components/results/FilterBar.tsx
@@ -1,15 +1,17 @@
-import {CurrentFilters} from "../../types";
 import React, {useContext} from "react";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import styled from "styled-components";
-import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
-import FormGroup from "@mui/material/FormGroup";
-import {filterGroups} from "./filterHelpers";
-import Button from "@mui/material/Button";
+import {
+  FormControlLabel,
+  Checkbox,
+  FormGroup,
+  FormControl,
+  FormLabel,
+  Button,
+} from "@mui/material";
 import {GlobalStateContext} from "../../context/globalStates";
-import { colors } from "../../theme";
+import {filterGroups} from "./filterHelpers";
+import {colors} from "../../theme";
+import {CurrentFilters} from "../../types";
 
 const Sidebar = styled.div<{isFilterOpen: boolean}>`
   display: flex;
@@ -38,58 +40,61 @@ const Sidebar = styled.div<{isFilterOpen: boolean}>`
     &:not(:first-child) {
       margin-top: 15px;
     }
-
   }
 
   @media (max-width: 752px) {
-    display: ${props => (props.isFilterOpen ? "flex" : "none")};
+    display: ${(props) => (props.isFilterOpen ? "flex" : "none")};
     background-color: white;
     position: relative;
     box-shadow: 0px 5px 5px -3px rgb(0 0 0 / 20%),
-    0px 8px 10px 1px rgb(0 0 0 / 14%),
-    0px 3px 14px 2px rgb(0 0 0 / 12%);
+      0px 8px 10px 1px rgb(0 0 0 / 14%), 0px 3px 14px 2px rgb(0 0 0 / 12%);
     padding: 30px;
   }
 `;
 
 export const FilterBar: React.FC = () => {
-    const {
-      isFilterOpen,
-      currentFilters,
-      handleFilterChange,
-      handleClearFilters
-    } = useContext(GlobalStateContext);
+  const {isFilterOpen, currentFilters, handleFilterChange, handleClearFilters} =
+    useContext(GlobalStateContext);
 
-    return (
-      <Sidebar isFilterOpen={isFilterOpen}>
-        {filterGroups.map(({groupName, groupLabel, filters}) => (
-          <FormControl key={groupName} component="fieldset">
-            <FormLabel component="legend">{groupLabel}</FormLabel>
-            <FormGroup>
-              {filters.map(({label, name}) => (
-                <FormControlLabel
-                  key={name}
-                  label={label}
-                  control={
-                    <Checkbox
-                      name={name}
-                      checked={
-                        !!currentFilters[
-                          groupName as keyof CurrentFilters
-                          ]?.includes(name)
-                      }
-                      onChange={handleFilterChange(groupName as keyof CurrentFilters)}
-                    />
-                  }
-                  sx={{fontSize: '.75rem', height: '1.25rem', marginBottom: '0.3125rem', marginTop: '0.3125rem', whiteSpace: 'nowrap', lineHeight: '1'}}
-                />
-              ))}
-            </FormGroup>
-          </FormControl>
-        ))}
-        <Button color="secondary" onClick={handleClearFilters}>
-          clear
-        </Button>
-      </Sidebar>);
-  }
-;
+  return (
+    <Sidebar isFilterOpen={isFilterOpen}>
+      {filterGroups.map(({groupName, groupLabel, filters}) => (
+        <FormControl key={groupName} component="fieldset">
+          <FormLabel component="legend">{groupLabel}</FormLabel>
+          <FormGroup>
+            {filters.map(({label, name}) => (
+              <FormControlLabel
+                key={name}
+                label={label}
+                control={
+                  <Checkbox
+                    name={name}
+                    checked={
+                      !!currentFilters[
+                        groupName as keyof CurrentFilters
+                      ]?.includes(name)
+                    }
+                    onChange={handleFilterChange(
+                      groupName as keyof CurrentFilters
+                    )}
+                  />
+                }
+                sx={{
+                  fontSize: ".75rem",
+                  height: "1.25rem",
+                  marginBottom: "0.3125rem",
+                  marginTop: "0.3125rem",
+                  whiteSpace: "nowrap",
+                  lineHeight: "1",
+                }}
+              />
+            ))}
+          </FormGroup>
+        </FormControl>
+      ))}
+      <Button color="secondary" onClick={handleClearFilters}>
+        clear
+      </Button>
+    </Sidebar>
+  );
+};
